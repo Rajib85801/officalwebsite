441f8adabf6ff01add44b888eb36b31b5491bfe3
Rajib Hasan <61510797+Rajib85801@users.noreply.github.com>
2022-03-03 11:50:56 -0600

    Update about.js

--- about.js
+++ about.js
@@ -1,8 +1,5 @@
-// API for get requests
 let url = fetch("https://jsonplaceholder.com/li/1");
-      
-// fetchRes is the promise to resolve
-// it by using.then() method
+
 url.then(res =>
     res.json()).then(d => {
         console.log(d)
